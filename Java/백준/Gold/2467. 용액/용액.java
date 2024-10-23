855a4740c17170a890771e2e109832054b19c892
YeonjeKo <48676371+YeonjeKo@users.noreply.github.com>
2024-10-24 01:33:53 +0900

    [Gold V] Title: 용액, Time: 332 ms, Memory: 28092 KB -BaekjoonHub

---
+++ Java/백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] solution = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			solution[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int idx1 = -1;
+		int idx2 = -1;
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < N - 1; i++) {
+			int idx = binarySearch(i + 1, N - 1, solution, -solution[i]);
+			int tmp = Math.abs(solution[i] + solution[idx]);
+			
+			if (tmp < min) {
+				idx1 = i;
+				idx2 = idx;
+				min = tmp;
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(solution[idx1]).append(" ").append(solution[idx2]);
+		System.out.println(sb);
+	}
+	
+	public static int binarySearch(int start, int end, int[] arr, int target) {
+		int i = start - 1;
+		int N = end + 1;
+		while (end >= start) {
+			int mid = (start + end) / 2;
+			
+			if (arr[mid] == target)
+				return mid;
+			
+			if (arr[mid] < target)
+				start = mid + 1;
+			else
+				end = mid - 1;
+		}
+		
+		if (start >= N)
+			start = N - 1;
+		
+		if (end <= i + 1)
+			end = i + 1;
+		
+		if (Math.abs(arr[i] + arr[start]) < Math.abs(arr[i] + arr[end]))
+			return start;
+		else
+			return end;
+	}
+
+}
