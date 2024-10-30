447e69b8b5384b1563e2c539e7ff3a5e34ca87d3
YeonjeKo <48676371+YeonjeKo@users.noreply.github.com>
2024-10-31 01:30:08 +0900

    [Bronze V] Title: 아스키 코드, Time: 116 ms, Memory: 14236 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println((int)s.next().charAt(0));
+	}
+}
