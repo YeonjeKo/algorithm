623b64ed8716322668a9ed3ba2d03ab773e81f47
YeonjeKo <48676371+YeonjeKo@users.noreply.github.com>
2025-11-06 22:33:09 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 100 MB -BaekjoonHub

--- Java/프로그래머스/1/42748. K번째수/K번째수.java
+++ Java/프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
+
         for (int i = 0; i < commands.length; i++) {
             ArrayList<Integer> arr = new ArrayList<>();
             for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
