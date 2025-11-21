90357c75692cbcc7403e42e4a2fc7f1aa4dcd96f
YeonjeKo <48676371+YeonjeKo@users.noreply.github.com>
2025-11-22 01:06:36 +0900

    [D3] Title: 햄버거 다이어트, Time: 164 ms, Memory: 30,416 KB -BaekjoonHub

---
+++ Java/SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,87 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+    static int N, L;
+    static int[][] ingredient;
+    static long max;
+    
+    public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			N = sc.nextInt();
+            L = sc.nextInt();
+            
+            ingredient = new int[N][2];
+            
+            for (int i = 0; i < N; i++) {
+             	for(int j = 0; j < 2; j++) {
+                 	ingredient[i][j] = sc.nextInt();   
+                }
+            }
+	
+            max = Long.MIN_VALUE;
+            dfs(0, 0, 0);
+            
+            System.out.println("#" + test_case + " " + max);
+           
+		}
+	}
+    
+    public static void dfs(int i, long totalScore, long totalCal) {
+            if (totalCal > L) {
+             	return;   
+            }
+            
+            if (i == N) {
+                if (max < totalScore)
+                    max = totalScore;
+                return;
+            }
+                
+            dfs(i + 1, totalScore + ingredient[i][0], totalCal + ingredient[i][1]);
+            dfs(i + 1, totalScore, totalCal);
+                
+    }
+}
